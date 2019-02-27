45341c9e304000929d3866b9c51c5e2086af31d3
SSMDFW <45538980+SSMDFW@users.noreply.github.com>
2019-02-27 11:54:26 -0600

    Update
    Edited and saved

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,1 +1,89 @@
+import csv
+import os
 
+pathcsv = os.path.join("C:\\Users\\silva\\Desktop\\LearnPython\\HomeworkSupport\\budget_data.csv")
+ 
+maxinc = 0
+maxdec = 0
+mthcount = 0
+nettotal = 0
+value = 0
+change = 0
+mon = []
+var = []
+
+with open(pathcsv, newline = "") as csvfile:
+    csvreader = csv.reader(csvfile, delimiter = ",")
+
+
+    csvheader = next(csvreader)
+
+    
+    firstline = next(csvreader)
+    mthcount += 1
+    nettotal += int(firstline[1])
+    pnl = int(firstline[1])
+    
+
+    for row in csvreader:
+
+        mon.append(row[0])
+        
+
+        change = int(row[1])-pnl
+        var.append(change)
+        pnl = int(row[1])
+        mthcount += 1
+        nettotal = nettotal + int(row[1])
+
+
+    maxinc = max(var)
+    maxloc = var.index(maxinc)
+    maxincd = mon[maxloc]
+
+
+    mininc = min(var)
+    minloc = var.index(mininc)
+    minincd = mon[minloc]
+
+
+    avgchg = sum(var)/len(var)
+
+
+
+
+l1 = "Financial Analysis"
+l2 = "---------------------"
+l3 = str(f"Total Months: {str(mthcount)}")
+l4 = str(f"Total: ${str(nettotal)}")
+l5 = str(f"Average Change: ${str(round(avgchg,2))}")
+l6 = str(f"Greatest Increase in Profits: {maxincd} (${str(maxinc)})")
+l7 = str(f"Greatest Decrease in Profits: {minincd} (${str(mininc)})")
+
+print(l1)
+print(l2)
+print(l3)
+print(l4)
+print(l5)
+print(l6)
+print(l7)
+
+
+file = open("C:\\Users\\silva\\Desktop\\LearnPython\\HomeworkSupport\\budget_data2.csv",'w')
+file.write(l1+'\n')
+file.write(l2+'\n')
+file.write(l3+'\n')
+file.write(l4+'\n')
+file.write(l5+'\n')
+file.write(l6+'\n')
+file.write(l7)
+
+file.close()
+
+#Financial Analysis
+#----------------------------
+#Total Months: 86
+#Total: $38382578
+#Average  Change: $-2315.12
+#Greatest Increase in Profits: Feb-2012 ($1926159)
+#Greatest Decrease in Profits: Sep-2013 ($-2196167)
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,1 +1,87 @@
+import csv
+import os
 
+pathcsv = os.path.join("C:\\Users\\silva\\Desktop\\LearnPython\\HomeworkSupport\\election_data.csv")
+ 
+cands = []
+votes = []
+voteper = []
+votettl = 0
+#The total number of votes cast
+#A complete list of candidates who received votes
+#The percentage of votes each candidate won
+#The total number of votes each candidate won
+#The winner of the election based on popular vote.
+
+with open(pathcsv, newline = "") as csvfile:
+    csvreader = csv.reader(csvfile, delimiter = ",")
+    csv_header = next(csvreader)
+
+    for row in csvreader:
+        votettl += 1 
+        if row[2] not in cands:
+            cands.append(row[2])
+            index = cands.index(row[2])
+            votes.append(1)
+        else:
+            index = cands.index(row[2])
+            votes[index] += 1
+    
+    for v in votes:
+        percentage = (v/votettl) * 100
+        percentage = round(percentage)
+        percentage = "%.3f%%" % percentage
+        voteper.append(percentage)
+    
+  
+    winning = max(votes)
+    index = votes.index(winning)
+    winner = cands[index]
+
+
+
+
+l1 = "Election Results"
+l2 = "-------------------------"
+l3 = str(f"Total Votes: {str(votettl)}")
+l4 =  "-------------------------"
+l6 =  "-------------------------"
+l7 = str(f"Winner: {winner}")
+l8 =  "-------------------------"
+
+print(l1)
+print(l2)
+print(l3)
+print(l4)
+for i in range(len(cands)):
+    print(f"{cands[i]}: {str(voteper[i])} ({str(votes[i])})")
+print(l6)
+print(l7)
+print(l8)
+
+
+file = open("C:\\Users\\silva\\Desktop\\LearnPython\\HomeworkSupport\\electiondata2.csv",'w')
+file.write(l1+'\n')
+file.write(l2+'\n')
+file.write(l3+'\n')
+file.write(l4+'\n')
+for i in range(len(cands)):
+    line = str(f"{cands[i]}: {str(voteper[i])} ({str(votes[i])})")
+    file.write('{}\n'.format(line))  
+file.write(l6+'\n')
+file.write(l7+'\n')
+file.write(l8+'\n')
+file.close()
+
+
+#  Election Results
+#  -------------------------
+#  Total Votes: 3521001
+#  -------------------------
+#  Khan: 63.000% (2218231)
+#  Correy: 20.000% (704200)
+#  Li: 14.000% (492940)
+#  O'Tooley: 3.000% (105630)
+#  -------------------------
+#  Winner: Khan
+#  -------------------------
